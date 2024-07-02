757c3f1fbd14e1f2ec18ea006bb71f9b94950886
Manoj-Katta <manojkatta1173@gmail.com>
2024-07-02 22:18:28 +0530

    updated details

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,14 +11,15 @@ export const EXPERIENCES = [
     year: "May 2024 - Present",
     role: "Research Intern",
     company: "City, University of London",
-    description: `Researched and analyzed C2PA implementations for deepfake detection. Implemented C2PA metadata to images for testing and comparison. Utilized various methods to identify false images using deepfake tools. Demonstrated methods for incorporating data into C2PA metadata, including watermarking and external repositories.`,
+    description: `Conducted research and analysis on Coalition for Content Provenance and Authenticity (C2PA) implementations for deepfake detection.
+Developed my own C2PA implementation using ReactJS and NodeJS SDK of Content Authenticity Initiative(CAI) that can achieve a 95% verification accuracy for C2PA manifests`,
     technologies: ["C2PA", "Deepfake Detection", "Image Processing", "Metadata"],
   },
   {
     year: "Apr 2024 - May 2024",
     role: "Web Development Intern",
-    company: "Suvidha Mahila Mandal",
-    description: `Collaborated with the Suvidha Foundation NGO team to revamp their website. Engineered the website using HTML, CSS, JavaScript, Bootstrap, and React. Designed a visually engaging and user-friendly interface. Implemented SEO best practices to improve online visibility.`,
+    company: "Suvidha Foundation",
+    description: `Collaborated with the Suvidha Foundation NGO team to revamp their website. Engineered the website using HTML, CSS, JavaScript, Bootstrap, and React. Implemented SEO best practices to improve online visibility.`,
     technologies: ["HTML", "CSS", "JavaScript", "Bootstrap", "React"],
   },
 ];
@@ -28,7 +29,7 @@ export const PROJECTS = [
     title: "Ascend",
     image: project1,
     description:
-      "Developed a customized test platform to assess various skills. Implemented personalized roadmaps for user upskilling and integrated ML models to recommend relevant articles based on user's scores. Designed a business plan and go-to-market strategy.",
+      "Developed a customized test platform to assess various Tech skills. Implemented personalized roadmaps, leading to a 20% increase in user growth. Integrated custom Machine Learning models to recommend relevant articles based on user score. Designed a comprehensive business plan and go-to-market strategy for implementing new ideas",
     technologies: ["React", "Node.js", "Javascript", "MongoDB", "Express.Js", "Tensor Flow"],
   },
   {
@@ -56,7 +57,7 @@ export const CERTIFICATIONS = [
 
 export const ADDITIONAL_DETAILS = {
   achievements: [
-    "Solved over 300 problems on Leetcode and GFG combined."
+    "Solved over 400+ problems on Leetcode and GFG combined."
   ],
   positionsOfResponsibility: [
     "Representative of Career and Development Services, IIT Jammu",
